19be5d829e0109974cfab84263e882c1026fc6df
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-04 14:26:49 -0400

    Update error-checker.py

--- error-checker.py
+++ error-checker.py
@@ -1,13 +1,13 @@
 import os
 
-hostapd = '/ext/hostapd/hostapd.conf'
-dnsmasq = '/etc/dnsmasq.conf'
+def check_config_files():
+    hostapd = '/etc/hostapd/hostapd.conf'
+    dnsmasq = '/etc/dnsmasq.conf'
 
-hostapd_exists = os.path.exists(hostapd)
-dnsmasq_exists = os.path.exists(dnsmasq)
+    hostapd_exists = os.path.exists(hostapd)
+    dnsmasq_exists = os.path.exists(dnsmasq)
 
-
-if hostapd_exists and dnsmasq_exists:
-    print("Both Hostapd.conf and Dnsmasq.conf are successfully created!")
-else:
-    print("Error! one or both files do not exist!")
+    if hostapd_exists and dnsmasq_exists:
+        print("Both hostapd.conf and dnsmasq.conf are successfully created!")
+    else:
+        print("Error! One or both files do not exist!")
