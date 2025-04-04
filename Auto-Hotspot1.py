4f9133f3ddb9cb0e578420b1f0ea49b3b4418990
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-03 20:28:51 -0400

    Deleted Pre-req
    Unnecessary due to already being a separate file

--- Auto-Hotspot1.py
+++ Auto-Hotspot1.py
@@ -96,32 +96,4 @@ def custom():
                                         "sudo systemctl status hostapd",
                                         "sudo systemctl status dnsmasq"])
 
-def prereq():
-    system = platform.system
-    if system == "Linux":
-        print("Script is compatible with Linux!")
-    elif system == 'Windows':
-        print('Script is not compatible with Windows')
-        sys.exit(0)
 
-    print("Checking for required hardware...")
-
-    devices = subprocess.check_output(['wlan', 'netsh', 'show', 'network'])
-
-    devices = devices.decode('ascii')
-    devices = devices.replace("\r","")
-
-    print(devices)
-
-    if devices == ('wlan0', 'wlan1'):
-        print("RPI can be configured into an AP!")
-    elif devices == ('wlan0'):
-        print("RPI must have 1 more Wi-Fi Adapter")
-
-def app_status():
-    try:
-        subprocess.run(['hostapd', '--version', 'dnsmasq', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-        return True
-    except (subprocess.CalledProcessError, FileNotFoundError):
-        return False
-    
