86c71bd166be247f1abf398e3d220994c717a072
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-04 14:26:09 -0400

    Update Uninstall.py

--- Uninstall.py
+++ Uninstall.py
@@ -1,12 +1,13 @@
 import subprocess
 import sys
 
-def question():
+def uninstall_hotspot():
     while True:
-        
         question = input("Would you really like to uninstall everything? (yes/no): ").strip().lower()
 
         if question == 'yes':
+            subprocess.run(["sudo", "systemctl", "stop", "hostapd"])
+            subprocess.run(["sudo", "systemctl", "stop", "dnsmasq"])
             subprocess.run(["sudo", "apt", "purge", "-y", "hostapd"])
             subprocess.run(["sudo", "apt", "purge", "-y", "dnsmasq"])
             subprocess.run(["sudo", "rm", "-rf", "/etc/hostapd/hostapd.conf"])
@@ -14,9 +15,7 @@ def question():
             print("Uninstallation complete.")
             break
         elif question == 'no':
-            print("Uninstalltion Terminated")
+            print("Uninstallation terminated.")
             sys.exit()
         else:
-            print("Error! No Answer Provided!")
-
-question()
+            print("Error! No valid answer provided!")
