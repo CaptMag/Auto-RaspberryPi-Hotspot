a542e53357aa4026a3b07e4773de53c2f7ebb6af
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-06 18:47:57 -0400

    Update Menu.py
    Expanded Menu with more options and proper running of functions as well as better organization

--- Menu.py
+++ Menu.py
@@ -5,22 +5,44 @@ def menu():
         print("What would you like to do?")
         print("[1] Create a Raspberry-Pi Hotspot")
         print("[2] Debug Raspberry-Pi")
-        print("[3] Check For File Creation")
-        print("[4] Uninstall Hotspot")
-        print("[5] Exit Menu")
+        print("[3] Uninstall Hotspot")
+        print("[4] Exit Menu")
 
         choice = input("Choose an option: ")
         if choice == "1":
-            from Compatibility import prereq
-            prereq()
+            print("[1] Check for device compatability")
+            print("[2] Run Installtion")
+            print("[3] Exit")
+            subchoice = input("Choose and option: ")
+            if subchoice == "1":
+                from Compatibility import prereq
+                prereq()
+            elif subchoice == "2":
+                from HotspotSetup import setup_hotspot
+                from NetMan import configure_network
+                from SysConf import configure_hostapd, configure_dnsmasq, setup_iptables
+                setup_hotspot()
+                configure_network()
+                configure_hostapd()
+                configure_dnsmasq()
+                setup_iptables()
+            elif subchoice == "3":
+                continue
         elif choice == "2":
-            from hostapd_diagnose import hostapd_issues
-            hostapd_issues()
+            print("[1] Check for file integrity")
+            print("[2] Debug Raspberry-Pi Issues")
+            print("[3] Exit")
+            subchoice = input("Choose an option: ")
+            if subchoice == "1":
+                from check_configs import check_config_files
+                check_config_files()
+            elif subchoice == "2":
+                from hostapd_diagnose import hostapd_issues
+                hostapd_issues()
+            elif choice == "3":
+                continue
         elif choice == "3":
-            from check_configs import check_config_files
-            check_config_files()
-        elif choice == "4":
             from Uninstall import uninstall_hotspot
             uninstall_hotspot()
-        elif choice == "5":
+        elif choice == "4":
             break
