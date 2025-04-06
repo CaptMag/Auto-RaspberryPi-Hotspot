de4850f82124cc4df86513a7aebd69de6cae5a99
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-06 18:50:33 -0400

    Update Uninstall.py
    Added more security for purging and better organization and error checker

--- Uninstall.py
+++ Uninstall.py
@@ -1,21 +1,35 @@
+# uninstall.py
 import subprocess
 import sys
+import os
 
 def uninstall_hotspot():
     while True:
         question = input("Would you really like to uninstall everything? (yes/no): ").strip().lower()
 
         if question == 'yes':
-            subprocess.run(["sudo", "systemctl", "stop", "hostapd"])
-            subprocess.run(["sudo", "systemctl", "stop", "dnsmasq"])
-            subprocess.run(["sudo", "apt", "purge", "-y", "hostapd"])
-            subprocess.run(["sudo", "apt", "purge", "-y", "dnsmasq"])
-            subprocess.run(["sudo", "rm", "-rf", "/etc/hostapd/hostapd.conf"])
-            subprocess.run(["sudo", "rm", "-rf", "/etc/dnsmasq.conf"])
+            print("Stopping Services...")
+            subprocess.run(["sudo", "systemctl", "stop", "hostapd"], check=True)
+            subprocess.run(["sudo", "systemctl", "stop", "dnsmasq"], check=True)
+
+            print("Removing Packages...")
+            subprocess.run(["sudo", "apt", "purge", "-y", "hostapd"], check=True)
+            subprocess.run(["sudo", "apt", "purge", "-y", "dnsmasq"], check=True)
+
+            print("Removing Configuration Files...")
+
+            hostapd = "/etc/hostapd/hostapd.conf"
+            dnsmasq = "/etc/dnsmasq.conf"
+
+            if os.path.exists(hostapd):
+                subprocess.run(["sudo", "rm", "-rf", "/etc/hostapd/hostapd.conf"], check=True)
+            if os.path.exists(dnsmasq):
+                subprocess.run(["sudo", "rm", "-rf", "/etc/dnsmasq.conf"], check=True)
+            
             print("Uninstallation complete.")
             break
         elif question == 'no':
             print("Uninstallation terminated.")
-            sys.exit()
+            return
         else:
             print("Error! No valid answer provided!")
