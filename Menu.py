bbf4bada0369676b107c8f6545c56eaf44352799
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-16 20:01:41 -0400

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,7 +1,8 @@
-def menu():
-    from HotspotSetup import setup_hotspot
+import os
 
+def menu():
     while True:
+        os.system('clear')
         print("What would you like to do?")
         print("[1] Create a Raspberry-Pi Hotspot")
         print("[2] Debug Raspberry-Pi")
@@ -10,39 +11,70 @@ def menu():
 
         choice = input("Choose an option: ")
         if choice == "1":
-            print("[1] Check for device compatability")
-            print("[2] Run Installtion")
-            print("[3] Exit")
-            subchoice = input("Choose and option: ")
-            if subchoice == "1":
-                from Compatibility import prereq
-                prereq()
-            elif subchoice == "2":
-                from HotspotSetup import setup_hotspot
-                from NetMan import configure_network
-                from SysConf import configure_hostapd, configure_dnsmasq, setup_iptables
-                setup_hotspot()
-                configure_network()
-                configure_hostapd()
-                configure_dnsmasq()
-                setup_iptables()
-            elif subchoice == "3":
-                continue
+            sub_choice1()
         elif choice == "2":
-            print("[1] Check for file integrity")
-            print("[2] Debug Raspberry-Pi Issues")
-            print("[3] Exit")
-            subchoice = input("Choose an option: ")
-            if subchoice == "1":
-                from check_configs import check_config_files
-                check_config_files()
-            elif subchoice == "2":
-                from hostapd_diagnose import hostapd_issues
-                hostapd_issues()
-            elif choice == "3":
-                continue
+            sub_choice2()
         elif choice == "3":
-            from Uninstall import uninstall_hotspot
-            uninstall_hotspot()
+            uninstallation()
         elif choice == "4":
             break
+
+def sub_choice1():
+    os.system('clear')
+    print("[1] Check for device compatability")
+    print("[2] Run Installtion")
+    print("[3] Exit")
+    subchoice = input("Choose an option: ")
+    
+    if subchoice == "1":
+        from Compatibility import prereq
+        prereq()
+    elif subchoice == "2":
+        creation()
+    elif subchoice == "3":
+        return  # Return to main menu instead of continue
+
+def creation():
+    os.system('clear')
+    from HotspotSetup import setup_hotspot
+    from NetMan import configure_network
+    from SysConf import configure_hostapd, configure_dnsmasq, setup_iptables
+    setup_hotspot()
+    configure_network()
+    configure_hostapd()
+    configure_dnsmasq()
+    setup_iptables()
+
+def sub_choice2():
+    os.system('clear')
+    print("[1] Check for file integrity")
+    print("[2] Debug Raspberry-Pi Issues")
+    print("[3] Exit")
+    subchoice = input("Choose an option: ")
+    
+    if subchoice == "1":
+        check_file()
+    elif subchoice == "2":
+        diagnosis()
+    elif subchoice == "3":
+        return  # Return to main menu
+
+def check_file():
+    os.system('clear')
+    from check_configs import check_config_files
+    check_config_files()
+
+def diagnosis():
+    os.system('clear')
+    from hostapd_diagnose import hostapd_issues
+    hostapd_issues()
+
+def uninstallation():
+    os.system('clear')
+    from Uninstall import uninstall_hotspot
+    uninstall_hotspot()
+
+
+# Call the menu function to start the program
+if __name__ == "__main__":
+    menu()
