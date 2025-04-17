77bf64f5c86f3142d1db4830c58ce62e9bccd89f
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-16 20:02:17 -0400

    Update check_configs.py

--- check_configs.py
+++ check_configs.py
@@ -1,6 +1,10 @@
 import os
+import time
 
 def check_config_files():
+
+    print('\n Checking For File Integrity \n')
+
     hostapd = '/etc/hostapd/hostapd.conf'
     dnsmasq = '/etc/dnsmasq.conf'
 
@@ -9,5 +13,7 @@ def check_config_files():
 
     if hostapd_exists and dnsmasq_exists:
         print("Both hostapd.conf and dnsmasq.conf are successfully created!")
+        time.sleep(3)
     else:
         print("Error! One or both files do not exist!")
+        time.sleep(3)
