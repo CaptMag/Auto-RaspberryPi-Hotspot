bfe43894c14bc4c44e734d9233bbcf01c122fe5f
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-04-20 19:25:32 -0400

    Update Uninstall.py
    Added removal of IP forwarding rules, firewall rules, fixed hostapd and dnsmasq removal, and nmcli rules. Add comments for better clarifications

--- Uninstall.py
+++ Uninstall.py
@@ -10,12 +10,12 @@ def uninstall_hotspot():
         if question == 'yes':
             print("\nStopping Services... \n")
             time.sleep(2)
-            subprocess.run(["sudo", "systemctl", "stop", "hostapd"], check=True)
-            subprocess.run(["sudo", "systemctl", "stop", "dnsmasq"], check=True)
+            subprocess.run(["sudo", "systemctl", "disable", "hostapd"], check=True) # Disables both Hostapd and Dnsmasq
+            subprocess.run(["sudo", "systemctl", "disable", "dnsmasq"], check=True)
 
             print("Removing Packages...")
             time.sleep(2)
-            subprocess.run(["sudo", "apt", "purge", "-y", "hostapd"], check=True)
+            subprocess.run(["sudo", "apt", "purge", "-y", "hostapd"], check=True) # Purges [removes] both applications
             subprocess.run(["sudo", "apt", "purge", "-y", "dnsmasq"], check=True)
 
             print("\nRemoving Configuration Files... \n")
@@ -24,9 +24,28 @@ def uninstall_hotspot():
             dnsmasq = "/etc/dnsmasq.conf"
 
             if os.path.exists(hostapd):
-                subprocess.run(["sudo", "rm", "-rf", "/etc/hostapd/hostapd.conf"], check=True)
+                subprocess.run(["sudo", "rm", "-rf", "/etc/hostapd/hostapd.conf"], check=True) # Deletes both files with root privilege
             if os.path.exists(dnsmasq):
                 subprocess.run(["sudo", "rm", "-rf", "/etc/dnsmasq.conf"], check=True)
+
+
+            print("\n Removing Network Manager Configurations... \n")
+
+            result = subprocess.run(["nmcli", "-t", "connection", "show"], capture_output=True, text=True) # Checks to see current network manager command line connections
+            for line in result.stdout.splitlines():
+                if "wifi" in line and "ap" in line:
+                    conn_name = line.split(":")[0]
+                    subprocess.run(["sudo", "nmcli", "connection", "delete", conn_name], check=False) # If an Access Point already exists, delete it
+
+
+            print("\n Resetting Network Forwarding... \n")
+            subprocess.run(["sudo", "sysctl", "-w", "net.ipv4.ip.forward=0"], check=False) # Sets IP forwarding back to default (false)
+
+
+            print("\n Cleaning Firewall Rules... \n")
+            subprocess.run(["sudo", "iptables", "-F"], check=False)
+            subprocess.run(["sudo", "iptables", "-t", "nat", "-F"], check=False) # Cleans any IPtables (firewall) rules
+
             
             print("Uninstallation complete.")
             time.sleep(1)
